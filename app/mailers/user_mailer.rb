4c8bef07e964aa44ee7ceefcf890edd4bb6d5688
Jiří Kvapil <jirikvapil@gmail.com>
2017-01-11 23:43:28 +0100

    změna mailů

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 	def birthday(users1,users2)
 		@users1=users1
 		@users2=users2
-		mail to: "kvapil@fme.vutbr.cz,vetesnikova@fme.vutbr.cz,sikorova@fme.vutbr.cz", subject: "Narozeniny"
+		mail to: "jirikvapil@centrum.cz,vetesnikova@fme.vutbr.cz,Hana.Hodosi@vut.cz", subject: "Narozeniny"
 		# mail to: "kvapil@fme.vutbr.cz", subject: "Narozeniny"
 	end
 end
